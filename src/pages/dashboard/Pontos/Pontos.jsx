e35f0809dabb0b00a404be6ee59f0b62ed60a986
Tiago Amorim <tiagoamorim1223@gmail.com>
2025-10-08 19:51:06 -0300

    novas atualizações da aba pontos, esta quase 100% proto

--- src/pages/dashboard/Pontos/Pontos.jsx
+++ src/pages/dashboard/Pontos/Pontos.jsx
@@ -1,5 +1,5 @@
 import "../Pontos/Pontos.css"
-import { FaBolt } from "react-icons/fa";
+import { FaBolt, FaTint, FaGift} from "react-icons/fa";
 
 function Pontos () {
     return (
@@ -48,27 +48,114 @@ function Pontos () {
                     </div>
                 </div>
 
+                {/* Energia */}
+
                 <div className="Basedescont">
                     <div className="Desconto">
                         <div className="Indesconto">
-                            <div className="Icondescont"><FaBolt/> </div>
-                            
+                            <div>
+                                <div style={{color:"yellow"}} className="Icondescont">
+                                    <FaBolt/>
+                                </div>
+                                
+                                <p className="Valordescont">
+                                    Desconto na Energia
+                                    <span> 1000 pontos</span>
+                                </p>
+                            </div>
+
                             <p>
-                                Desconto na Energia
-                                <span>1000 pontos</span>
+                                Reduz a conta de luz em até 15%
                             </p>
 
+                            <input type="button" value="Resgate" />
+
                         </div>
-
                     </div>
 
-                    <div>
+                    {/* Água */}
 
+                    <div className="Desconto">
+                        <div className="Indesconto">
+                            <div>
+                                <div style={{color:"#2563EB"}} className="Icondescont">
+                                    <FaTint/>
+                                </div>
+                                
+                                <p className="Valordescont">
+                                    Desconto na Água
+                                    <span> 800 pontos</span>
+                                </p>
+                            </div>
+
+                            <p>
+                                Economize na conta de água até 10%
+                            </p>
+
+                            <input type="button" value="Resgate" />
+
+                        </div>
                     </div>
 
-                    <div>
+                    {/* Vale compras */}
 
+                    <div className="Desconto">
+                        <div className="Indesconto">
+                            <div>
+                                <div className="Icondescont">
+                                    <FaGift/>
+                                </div>
+                                
+                                <p className="Valordescont">
+                                    Vale compras
+                                    <span> 1500 pontos</span>
+                                </p>
+                            </div>
+
+                            <p>
+                                vale de R$ 50 em supermercados parceiros
+                            </p>
+
+                            <input type="button" value="Resgate" />
+
+                        </div>
                     </div>
+                    
+                    <div className="Impacto">
+                        <div className="Ininpacto">
+                            <h2>Seu impacto ambiental</h2>
+
+                            <div>
+                                <div>
+                                    <p className="Impactonum" style={{color:"#16A34A"}}>
+                                        "15.2kg"
+                                    </p>
+                                    <p className="Impactoitens">
+                                        Material Reciclado
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <p className="Impactonum" style={{color:"#2563EB"}}>
+                                        "8.5L"
+                                    </p>
+                                     <p className="Impactoitens">
+                                        Óleo Coletado
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <p className="Impactonum" style={{color:"purple"}}>
+                                        "12"
+                                    </p>
+                                     <p className="Impactoitens">
+                                        Coletas realizadas
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
